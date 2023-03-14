cdea725eedf75136118d307391c7b5343a9343d2
korneev.dev <korneev.dev@gmail.com>
2023-03-14 23:06:56 +0300

    Implement tab layout

--- app/src/main/java/com/mik0war/complimentsApp/presentation/MainActivity.kt
+++ app/src/main/java/com/mik0war/complimentsApp/presentation/MainActivity.kt
@@ -3,6 +3,8 @@ package com.mik0war.complimentsApp.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.mik0war.complimentsApp.R
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +15,10 @@ class MainActivity : AppCompatActivity() {
 
         val viewPager = findViewById<ViewPager2>(R.id.view_pager)
         viewPager.adapter = PagerAdapter(this)
+
+        val tabLayout = findViewById<TabLayout>(R.id.tab_layout)
+        TabLayoutMediator(tabLayout, viewPager){tab, position ->
+            tab.text = getString(if (position == 0) R.string.compliments else R.string.quotes)
+        }.attach()
     }
 }
