6dc69a129e52a792643a5430e663826a03cb1c73
korneev.dev <korneev.dev@gmail.com>
2023-03-16 22:52:18 +0300

    Fix ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/mik0war/complimentsApp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/mik0war/complimentsApp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.mik0war.jokesapp", appContext.packageName)
+        assertEquals("com.mik0war.complimentsApp", appContext.packageName)
     }
 }
